cd43d70eff150f362be6f110df64a2ac0d987601
Nguyen Minh <minhntq237@gmail.com>
2022-05-07 22:37:04 +0700

    new WebSocket

---
+++ webSocket.js
@@ -0,0 +1,48 @@
+const WebSocket = require('ws');
+const webSocketServer = new WebSocket.Server({ port:8500 });
+
+let phoneClient = []
+let desktopClient = []
+
+webSocketServer.on('connection', (webSocketConnection) => {
+    console.log("websocket server is running")
+
+	webSocketConnection.on('message', async (data) => {
+		let parsedData = JSON.parse(data)
+
+		let message = parsedData.message
+		let content = parsedData.content
+
+		console.log('received: %s', message);
+
+		if (message == "hello, this is phone client") {
+			phoneClient.push(webSocketConnection)
+            sendMessageAndContentToAllClients(phoneClient, "nice to meet you, phone client", "")
+		}
+
+        if (message == "hello, this is desktop client") {
+			desktopClient.push(webSocketConnection)
+		}
+
+		if (message == "Orientation Data") {
+			console.log("Alpha", content[3])
+			console.log("Beta", content[4])
+			console.log("Gamma", content[5])
+		}
+    
+    });
+
+	webSocketConnection.on('close', () => {
+		removeElementFromArray(webSocketConnection, imageSubscribingClients)
+		removeElementFromArray(webSocketConnection, capturedImageSubscribingClients)
+	});
+
+});
+
+
+function sendMessageAndContentToAllClients(listOfClients, thisMessage, thisContent) {
+	console.log("SERVER SENDING MESSAGE TO WINDOWS")
+	listOfClients.forEach((client) => {
+		client.send(JSON.stringify({message: thisMessage, content: thisContent}));
+	})
+}
